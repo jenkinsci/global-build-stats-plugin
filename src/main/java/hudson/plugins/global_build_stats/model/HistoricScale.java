8011faa8eea231cba556c9e463a06948df6a3a72
fcamblor <fcamblor@dev.java.net>
2010-02-22 21:33:18 +0000

    rounding HistoricScales for display

--- src/main/java/hudson/plugins/global_build_stats/model/HistoricScale.java
+++ src/main/java/hudson/plugins/global_build_stats/model/HistoricScale.java
@@ -6,26 +6,41 @@ public enum HistoricScale {
 	YEARLY(Calendar.YEAR),
 	MONTHLY(Calendar.MONTH),
 	WEEKLY(Calendar.WEEK_OF_YEAR),
-	DAILY(Calendar.DAY_OF_YEAR),
-	HOURLY(Calendar.HOUR);
+	DAILY(Calendar.DAY_OF_YEAR, new Integer[]{ Calendar.HOUR, Calendar.MINUTE }),
+	HOURLY(Calendar.HOUR, new Integer[]{ Calendar.MINUTE });
 	
 	private int calendarField;
+	private Integer[] fieldsToReset;
+	
 	
 	private HistoricScale(int _calendarField){
+		this(_calendarField, new Integer[0]);
+	}
+	
+	private HistoricScale(int _calendarField, Integer[] _fieldsToReset){
 		this.calendarField = _calendarField;
+		this.fieldsToReset = _fieldsToReset;
 	}
 	
 	public Calendar getPreviousStep(Calendar currentStep){
-		return step(currentStep, -1);
-	}
-	
-	public Calendar getNextStep(Calendar currentStep){
-		return step(currentStep, 1);
-	}
-	
-	public Calendar step(Calendar currentDate, int step){
-		Calendar previousStep = (Calendar)currentDate.clone();
-		previousStep.add(calendarField, step);
+		Calendar previousStep = (Calendar)currentStep.clone();
+		
+		// Rounding date
+		boolean fieldToResetAlreadyReseted = true;
+		int i=0;
+		while(fieldToResetAlreadyReseted && i<fieldsToReset.length){
+			fieldToResetAlreadyReseted = currentStep.get(fieldsToReset[i])==0;
+			i++;
+		}
+		
+		if(fieldToResetAlreadyReseted){
+			previousStep.add(calendarField, -1);
+		} else {
+			for(i=0; i<fieldsToReset.length; i++){
+				previousStep.set(fieldsToReset[i], 0);
+			}
+		}
+		
 		return previousStep;
 	}
 }
