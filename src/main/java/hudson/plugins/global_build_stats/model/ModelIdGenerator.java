8d1b4bde6d901ee0378322bf40953fd2ef0da230
fcamblor <fcamblor@dev.java.net>
2010-07-17 16:40:38 +0000

    removed special web characters from being generated via ModelIdGenerator

--- src/main/java/hudson/plugins/global_build_stats/model/ModelIdGenerator.java
+++ src/main/java/hudson/plugins/global_build_stats/model/ModelIdGenerator.java
@@ -13,12 +13,13 @@ public class ModelIdGenerator {
 	public static final ModelIdGenerator INSTANCE = new ModelIdGenerator();
 	
 	private static final int KEY_DEPTH = 32;
+	// Don't put here special URL characters ! (%, ?, &, =, # etc..)
 	private static final char[] AVAILABLE_CHARS = new char[]{
 		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 
 		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
 		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
 		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
-		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '@', '$', '*', '%', '!', '#'
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '@', '$', '*', '!'
 	};
 	
 	private Map<Class, Set<String>> registeredIds = Collections.synchronizedMap(new HashMap<Class, Set<String>>());
