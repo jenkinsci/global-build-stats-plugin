bbf6ee8b89bb58f7941e8ea8e09c0a3f13dee396
fcamblor <fcamblor@dev.java.net>
2010-09-19 20:35:59 +0000

    Minor internationalized Message key rename

--- src/test/java/hudson/plugins/global_build_stats/BuildStatChartDataCreationTest.java
+++ src/test/java/hudson/plugins/global_build_stats/BuildStatChartDataCreationTest.java
@@ -1,12 +1,16 @@
 package hudson.plugins.global_build_stats;
 
+import hudson.plugins.global_build_stats.model.AbstractBuildStatChartDimension;
 import hudson.plugins.global_build_stats.model.BuildStatChartData;
+import hudson.plugins.global_build_stats.model.BuildStatConfiguration;
 import hudson.plugins.global_build_stats.model.DateRange;
 import hudson.util.DataSetBuilder;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import org.junit.Test;
 
@@ -14,15 +18,19 @@ public class BuildStatChartDataCreationTest {
 	
 	@Test
 	public void testBuildStatChartDataInstanciation(){
+		
+		BuildStatConfiguration config = new BuildStatConfiguration();
 		DataSetBuilder<String, DateRange> dsb = new DataSetBuilder<String, DateRange>();
+		List<AbstractBuildStatChartDimension> dimensions = new ArrayList<AbstractBuildStatChartDimension>();
+		dimensions.add(new AbstractBuildStatChartDimension.BuildCounterChartDimension(config, dsb));
 		
 		Calendar firstDay = new GregorianCalendar(2010, 0, 1);
 		Calendar secondDay = new GregorianCalendar(2010, 0, 2);
 		DateRange dr = new DateRange(firstDay, secondDay, new SimpleDateFormat("dd/MM/yyyy"));
-		dsb.add(1, Messages.Build_Results_Statuses_5_SUCCESS(), dr);
-		dsb.add(2, Messages.Build_Results_Statuses_4_FAILURES(), dr);
+		dsb.add(1, Messages.Build_Results_Statuses_SUCCESS(), dr);
+		dsb.add(2, Messages.Build_Results_Statuses_FAILURES(), dr);
 		
 		// Calling constructor
-		new BuildStatChartData(dsb);
+		new BuildStatChartData(dimensions);
 	}
 }
