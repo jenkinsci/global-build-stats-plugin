82ff1e8ba27a8697c5bd0a3a507d3f4bddca7463
fcamblor <fcamblor@dev.java.net>
2010-11-01 23:39:46 +0000

    provided ways to retrieve user who launched the build in JobBuildResultFactory

--- src/main/java/hudson/plugins/global_build_stats/JobBuildResultFactory.java
+++ src/main/java/hudson/plugins/global_build_stats/JobBuildResultFactory.java
@@ -2,12 +2,16 @@ package hudson.plugins.global_build_stats;
 
 import hudson.model.Result;
 import hudson.model.AbstractBuild;
+import hudson.model.Cause;
+import hudson.model.Cause.UserCause;
+import hudson.model.CauseAction;
 import hudson.plugins.global_build_stats.model.BuildResult;
 import hudson.plugins.global_build_stats.model.JobBuildResult;
 
 public class JobBuildResultFactory {
 
 	public static final JobBuildResultFactory INSTANCE = new JobBuildResultFactory();
+	private static final String SYSTEM_USERNAME = "SYSTEM";
 	
 	private JobBuildResultFactory(){
 	}
@@ -21,7 +25,30 @@ public class JobBuildResultFactory {
 			buildName = ((MavenModuleSet)build.getProject()).getRootModule().toString();
 		}*/
     	return new JobBuildResult(createBuildResult(build.getResult()), buildName, 
-    			build.getNumber(), build.getTimestamp(), duration, nodeName);
+    			build.getNumber(), build.getTimestamp(), duration, nodeName, extractUserNameIn(build));
+	}
+	
+	public static String extractUserNameIn(AbstractBuild build){
+		String userName = null;
+		UserCause uc = retrieveUserCause(build);
+		if(uc != null){
+			userName = uc.getUserName();
+		// If no UserCause has been found, SYSTEM user should have launched the build
+		} else {
+			userName = SYSTEM_USERNAME;
+		}
+		return userName;
+	}
+	
+	private static UserCause retrieveUserCause(AbstractBuild build){
+		for(CauseAction a : build.getActions(CauseAction.class)){
+			for(Cause c : a.getCauses()){
+				if(c instanceof UserCause){
+					return (UserCause)c;
+				}
+			}
+		}
+		return null;
 	}
 	
 	public BuildResult createBuildResult(Result result){
