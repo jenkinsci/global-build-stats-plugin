1a1b2d5bb324312d173cb3a96014a6ee2d4112dd
fcamblor <fcamblor@dev.java.net>
2010-11-02 00:26:17 +0000

    internationalized week & day character on charts

--- src/main/java/hudson/plugins/global_build_stats/model/HistoricScale.java
+++ src/main/java/hudson/plugins/global_build_stats/model/HistoricScale.java
@@ -5,6 +5,9 @@ import hudson.plugins.global_build_stats.Messages;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 
 public enum HistoricScale {
 	HOURLY(Calendar.HOUR_OF_DAY, new Integer[]{ Calendar.MINUTE }, "EEE HH:mm"){
@@ -19,40 +22,43 @@ public enum HistoricScale {
 	}, DAILY_FROM_NOW(Calendar.DAY_OF_YEAR, "EEE dd HH'h'"){
 		@Override public String getUnitLabel() { return Messages.Historic_Scales_Unit_Labels_days(); }
 		@Override public String getLabel() { return Messages.Historic_Scales_Labels_Daily_From_Now(); }
-	}, WEEKLY(Calendar.WEEK_OF_YEAR, new Integer[]{ Calendar.DAY_OF_WEEK, Calendar.HOUR_OF_DAY, Calendar.MINUTE}, "'W'w, EEE"){
+	}, WEEKLY(Calendar.WEEK_OF_YEAR, new Integer[]{ Calendar.DAY_OF_WEEK, Calendar.HOUR_OF_DAY, Calendar.MINUTE}, "%W%w, EEE"){
 		@Override public String getUnitLabel() { return Messages.Historic_Scales_Unit_Labels_weeks(); }
 		@Override public String getLabel() { return Messages.Historic_Scales_Labels_Weekly(); }
-	}, WEEKLY_FROM_NOW(Calendar.WEEK_OF_YEAR, "'W'w, EEE"){
+		@Override public Map<String,String> getDateRangeFormatterLocalizedConversionMap(){ return new HashMap<String, String>(){{ put("%W%", Messages.Historic_Scales_Unit_Labels_weekChar()); }}; }
+	}, WEEKLY_FROM_NOW(Calendar.WEEK_OF_YEAR, "%W%w, EEE"){
 		@Override public String getUnitLabel() { return Messages.Historic_Scales_Unit_Labels_weeks(); }
 		@Override public String getLabel() { return Messages.Historic_Scales_Labels_Weekly_From_Now(); }
+		@Override public Map<String,String> getDateRangeFormatterLocalizedConversionMap(){ return new HashMap<String, String>(){{ put("%W%", Messages.Historic_Scales_Unit_Labels_weekChar()); }}; }
 	}, MONTHLY(Calendar.MONTH, new Integer[]{ Calendar.DAY_OF_MONTH, Calendar.HOUR_OF_DAY, Calendar.MINUTE}, "dd MMM ''yy"){
 		@Override public String getUnitLabel() { return Messages.Historic_Scales_Unit_Labels_monthes(); }
 		@Override public String getLabel() { return Messages.Historic_Scales_Labels_Monthly(); }
 	}, MONTHLY_FROM_NOW(Calendar.MONTH, "dd MMM ''yy"){
 		@Override public String getUnitLabel() { return Messages.Historic_Scales_Unit_Labels_monthes(); }
 		@Override public String getLabel() { return Messages.Historic_Scales_Labels_Monthly_From_Now(); }
-	}, YEARLY(Calendar.YEAR, new Integer[]{ Calendar.MONTH, Calendar.DAY_OF_YEAR, Calendar.HOUR_OF_DAY, Calendar.MINUTE}, "''yy 'd'D"){
+	}, YEARLY(Calendar.YEAR, new Integer[]{ Calendar.MONTH, Calendar.DAY_OF_YEAR, Calendar.HOUR_OF_DAY, Calendar.MINUTE}, "''yy %d%D"){
 		@Override public String getUnitLabel() { return Messages.Historic_Scales_Unit_Labels_years(); }
 		@Override public String getLabel() { return Messages.Historic_Scales_Labels_Yearly(); }
-	}, YEARLY_FROM_NOW(Calendar.YEAR, "''yy 'd'D"){
+		@Override public Map<String,String> getDateRangeFormatterLocalizedConversionMap(){ return new HashMap<String, String>(){{ put("%d%", Messages.Historic_Scales_Unit_Labels_dayChar()); }}; }
+	}, YEARLY_FROM_NOW(Calendar.YEAR, "''yy %d%D"){
 		@Override public String getUnitLabel() { return Messages.Historic_Scales_Unit_Labels_years(); }
 		@Override public String getLabel() { return Messages.Historic_Scales_Labels_Yearly_From_Now(); }
+		@Override public Map<String,String> getDateRangeFormatterLocalizedConversionMap(){ return new HashMap<String, String>(){{ put("%d%", Messages.Historic_Scales_Unit_Labels_dayChar()); }}; }
 	};
 	
 	private int calendarField;
 	private Integer[] fieldsToReset;
-	private DateFormat dateRangeFormatter;
+	private String dateRangeFormatterPattern;
 	
 	
 	private HistoricScale(int _calendarField, String dateRangeFormatterPattern){
 		this(_calendarField, new Integer[0], dateRangeFormatterPattern);
 	}
 	
-	private HistoricScale(int _calendarField, Integer[] _fieldsToReset, String dateRangeFormatterPattern){
+	private HistoricScale(int _calendarField, Integer[] _fieldsToReset, String _dateRangeFormatterPattern){
 		this.calendarField = _calendarField;
 		this.fieldsToReset = _fieldsToReset;
-		// TODO: internationalize this !
-		this.dateRangeFormatter = new SimpleDateFormat(dateRangeFormatterPattern);
+		this.dateRangeFormatterPattern = _dateRangeFormatterPattern;
 	}
 	
 	public Calendar getPreviousStep(Calendar currentStep){
@@ -94,9 +100,18 @@ public enum HistoricScale {
 	}
 
 	public DateFormat getDateRangeFormatter() {
-		return dateRangeFormatter;
+		String localizedPattern = this.dateRangeFormatterPattern;
+		for(Entry<String, String> entry : getDateRangeFormatterLocalizedConversionMap().entrySet()){
+			localizedPattern = localizedPattern.replaceAll(entry.getKey(), "'"+entry.getValue()+"'");
+		}
+		return new SimpleDateFormat(localizedPattern);
 	}
 
+	public Map<String,String> getDateRangeFormatterLocalizedConversionMap(){
+		// By default, don't localize anything here !
+		return new HashMap<String, String>();
+	}
+	
 	public abstract String getLabel();
 	public abstract String getUnitLabel();
 }
