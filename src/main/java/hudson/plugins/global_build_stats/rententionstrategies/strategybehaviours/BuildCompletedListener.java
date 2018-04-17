6463dbee3b4c5c9e472a814adf972dd0e3c85896
Rafal Prasal <rafal.prasal@gmail.com>
2018-04-17 20:41:45 +0200

    JENKINS-43183 Can't see pipeline jobs in Global Build Stats

--- src/main/java/hudson/plugins/global_build_stats/rententionstrategies/strategybehaviours/BuildCompletedListener.java
+++ src/main/java/hudson/plugins/global_build_stats/rententionstrategies/strategybehaviours/BuildCompletedListener.java
@@ -1,11 +1,11 @@
 package hudson.plugins.global_build_stats.rententionstrategies.strategybehaviours;
 
-import hudson.model.AbstractBuild;
+import hudson.model.Run;
 import hudson.plugins.global_build_stats.business.GlobalBuildStatsPluginSaver;
 
 /**
  * @author fcamblor
  */
 public interface BuildCompletedListener {
-    public void buildCompleted(AbstractBuild buils, GlobalBuildStatsPluginSaver pluginSaver);
+    public void buildCompleted(Run buils, GlobalBuildStatsPluginSaver pluginSaver);
 }
